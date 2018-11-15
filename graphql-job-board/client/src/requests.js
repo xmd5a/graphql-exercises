3d85de1f8d33dc34cba579fc56aba102c5403f77
xmd5a <kontakt@piotrszarmach.com>
2018-11-15 20:13:21 +0100

    get single job offer using parameter

--- graphql-job-board/client/src/requests.js
+++ graphql-job-board/client/src/requests.js
@@ -21,3 +21,29 @@ export async function loadJobs() {
   const responseBody = await response.json();
   return responseBody.data.jobs;
 }
+
+// return single job
+export async function loadJob(id) {
+  const response = await fetch(endpointUrl, {
+    method: 'POST',
+    headers: {'content-type': 'application/json'},
+    body: JSON.stringify({
+      query: `query JobQuery($id: ID!){
+        job(id: $id) {
+          id
+          title
+          company {
+            id
+            name
+            description
+          }
+          description
+        }
+      }`,
+      variables: {id}
+    })
+  });
+
+  const responseBody = await response.json();
+  return responseBody.data.job;
+}
